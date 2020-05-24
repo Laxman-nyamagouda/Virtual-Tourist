ea6bf9ffcc03fc97b76bdff7461493cd908b802b
Laxman Nyamagouda <laxman.nyamagouda@gmail.com>
2020-05-24 12:21:39 -0400

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Laxman Nyamagouda on 5/24/20.
+//  Copyright © 2020 Laxman Nyamagouda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
